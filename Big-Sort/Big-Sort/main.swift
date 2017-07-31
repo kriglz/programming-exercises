d25232c766f38ccc189c4560b03d4c86ccaf5026
Kristina Gelzinyte <kriglz@Kristinas-MacBook-Pro.local>
2017-07-31 11:05:38 +0300

    init project

---
+++ Big-Sort/Big-Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Big-Sort
+//
+//  Created by Kristina Gelzinyte on 7/31/17.
+//  Copyright © 2017 Kristina Gelzinyte. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
