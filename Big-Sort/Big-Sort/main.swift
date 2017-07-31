2bdc81d1a2509678be589576b6f3df3c7925ab57
Kristina Gelzinyte <kriglz@Kristinas-MacBook-Pro.local>
2017-07-31 11:14:53 +0300

    for loop for redlines()

--- Big-Sort/Big-Sort/main.swift
+++ Big-Sort/Big-Sort/main.swift
@@ -1,3 +1,4 @@
+// https://www.hackerrank.com/challenges/big-sorting
 //
 //  main.swift
 //  Big-Sort
@@ -6,7 +7,33 @@
 //  Copyright © 2017 Kristina Gelzinyte. All rights reserved.
 //
 
+/*
+ Sample Input 0
+ 
+ 6
+ 31415926535897932384626433832795
+ 1
+ 3
+ 10
+ 3
+ 5
+ 
+ Sample Output 0
+ 1
+ 3
+ 3
+ 5
+ 10
+ 31415926535897932384626433832795
+ */
+
 import Foundation
 
-print("Hello, World!")
+let n = Int(readLine()!.trimmingCharacters(in: .whitespacesAndNewlines))!
+
+for _ in 0...n {
+    print(readLine()!)
+}
+
+
 
