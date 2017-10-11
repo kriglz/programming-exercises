2ea86775bbaf4993068b1c20d01708b4e56e6040
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2017-10-11 16:40:49 +0300

    implemented progress bar for wkwebview load

--- Smash-Tag/Smash-Tag/WebViewController.swift
+++ Smash-Tag/Smash-Tag/WebViewController.swift
@@ -12,20 +12,59 @@ import WebKit
 class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     var webURL: URL?
-    
-    var webView: WKWebView! 
-    
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        view = webView
-    }
+    var webView: WKWebView!
+    var progressView = UIProgressView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView = WKWebView(frame: self.view.frame)
         let myRequest = URLRequest(url: webURL!)
-        webView?.load(myRequest)
+        webView.load(myRequest)
+        webView.navigationDelegate = self
+        view = webView
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView = UIProgressView(frame: CGRect(origin: CGPoint.init(x: 0, y: 64), size: CGSize(width: self.view.frame.width, height: 50.0)))
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress) , options: .new, context: nil)
+        webView.addSubview(progressView)
+        
+        webView.uiDelegate = self
+    }
+
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.navigationItem.title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if !(navigationAction.targetFrame?.isMainFrame != nil) {
+            webView.load(navigationAction.request)
+        }
+        return nil
+    }
+    
+        
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "estimatedProgress" {
+            self.progressView.alpha = 1.0
+            progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+        }
+        
+        if self.progressView.progress >= 1.0 {
+            UIView.animate(withDuration: 0.3,
+                           delay: 0.1,
+                           options: .curveEaseIn,
+                           animations:
+                {() -> Void in self.progressView.alpha = 0.0},
+                           completion:
+                {(finished: Bool) -> Void in self.progressView.alpha = 0.0})
+        }
     }
 }
