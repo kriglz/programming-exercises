575af6e97f8cd4697bd3a1122e889aba9311c757
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2017-10-21 10:39:27 +0300

    added timer for blinking eyes

---
+++ Faceit/Faceit/BlinkingFaceViewController.swift
@@ -0,0 +1,46 @@
+//
+//  BlinkingFaceViewController.swift
+//  Faceit
+//
+//  Created by Kristina Gelzinyte on 10/21/17.
+//  Copyright © 2017 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class BlinkingFaceViewController: FaceViewController {
+
+    var blinking = false {
+        didSet {
+            blinkIfNeeded()
+        }
+    }
+    
+    private struct BlinkRate {
+        static let closedDuration: TimeInterval = 0.4
+        static let openDuration: TimeInterval = 2.5
+    }
+    
+    private func blinkIfNeeded() {
+        if blinking {
+            faceView.eyesOpen = false
+            Timer.scheduledTimer(withTimeInterval: BlinkRate.closedDuration, repeats: false) { [weak self] timer in
+                self?.faceView.eyesOpen = true
+                Timer.scheduledTimer(withTimeInterval: BlinkRate.openDuration, repeats: false) { [weak self] timer in
+                    self?.blinkIfNeeded()
+                }
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        blinking = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        blinking = false
+    }
+    
+}
