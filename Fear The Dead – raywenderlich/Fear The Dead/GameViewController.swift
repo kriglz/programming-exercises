d3b0c4f3348ff4cefd834e1dbad6e375e0f1700f
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2017-12-06 11:16:22 -0800

    Init file updated to swift 4

---
+++ Fear The Dead – raywenderlich/Fear The Dead/GameViewController.swift
@@ -0,0 +1,74 @@
+//
+//  GameViewController.swift
+//  Fear The Dead
+//
+//  Created by Morten Faarkrog on 08/09/15.
+//  Copyright (c) 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    
+    class func unarchiveFromFile(file : String) -> SKNode? {
+        if let path = Bundle.main.path(forResource: file, ofType: "sks") {
+            let sceneData = try! NSData(contentsOfFile: path, options: .mappedIfSafe)
+            let archiver = NSKeyedUnarchiver(forReadingWith: sceneData as Data)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+    
+}
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let scene = GameScene.unarchiveFromFile(file: "GameScene") as? GameScene {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return UIInterfaceOrientationMask.allButUpsideDown
+        } else {
+            return UIInterfaceOrientationMask.all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
