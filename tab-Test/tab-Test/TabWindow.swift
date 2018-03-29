c7b1b1ccc7fb0471681f4de6a4de4350d9bbff2f
Kristina Gelžinytė <kristina.gelzinyte@gmail.com>
2018-03-29 09:54:53 +0300

    fixed VC showing

--- tab-Test/tab-Test/TabWindow.swift
+++ tab-Test/tab-Test/TabWindow.swift
@@ -25,8 +25,8 @@ class TabWindow: NSWindow, NSToolbarDelegate {
     
     var toolbarTabsIdentifierArray: [NSToolbarItem.Identifier] = []
     
-//    var currentView = ""
-//    var currentViewController = NSViewController()
+    var currentView = ""
+    var currentViewController = NSViewController()
     
     convenience init() {
         self.init(contentRect: NSRect(x: 100, y: 100, width: 600, height: 600),
@@ -44,17 +44,10 @@ class TabWindow: NSWindow, NSToolbarDelegate {
         }
         
         let toolbarz = NSToolbar(identifier: NSToolbar.Identifier(rawValue: "TestToolbar"))
-        
-//        var index = 0
-//        for itemId in toolbarTabsIdentifierArray {
-//
-//            self.toolbar?.insertItem(withItemIdentifier: itemId, at: index)
-//            index += 1
-//        }
-//
         toolbarz.isVisible = true
         toolbarz.allowsUserCustomization = true
         toolbarz.delegate = self
+        toolbarz.sizeMode = .regular
         
         self.toolbar = toolbarz
     }
@@ -71,10 +64,9 @@ class TabWindow: NSWindow, NSToolbarDelegate {
         }
         
         let toolbarItem = NSToolbarItem(itemIdentifier: itemIdentifier)
-//        toolbarItem.label = infoDictionary["title"]!
         toolbarItem.image = NSImage(named: NSImage.Name(rawValue: (infoDictionary["icon"]?.description)!))
         toolbarItem.target = self
-//        toolbarItem.action = #selector(viewSelected)
+        toolbarItem.action = #selector(viewSelected)
         
         return toolbarItem
     }
@@ -91,49 +83,49 @@ class TabWindow: NSWindow, NSToolbarDelegate {
         return toolbarTabsIdentifierArray
     }
     
-//    @objc func viewSelected(sender: NSToolbarItem) {
-//        self.loadViewWithIdentifier(viewTabIdentifier: sender.itemIdentifier.rawValue, withAnimation: true)
-//    }
-//
-//    func loadViewWithIdentifier(viewTabIdentifier: String, withAnimation shouldAnimate:Bool) {
-//        guard currentView == viewTabIdentifier else { return }
-//
-//        currentView = viewTabIdentifier
-//
-//        var infoDictionary = [String: String]()
-//
-//        for dictionary in toolbarTabsArray {
-//            if dictionary["identifier"] == viewTabIdentifier {
-//                infoDictionary = dictionary
-//                break
-//            }
-//        }
-//
-//        let className = infoDictionary["class"]!
-//
-//        switch className {
-//        case "First":
-//            currentViewController = First(nibName: NSNib.Name(rawValue: "First"), bundle: nil)
-//
-//        case "Second":
-//            currentViewController = Second(nibName: NSNib.Name(rawValue: "Second"), bundle: nil)
-//
-//        case "Third":
-//            currentViewController = Third(nibName: NSNib.Name(rawValue: "Third"), bundle: nil)
-//
-//        default:
-//            break
-//        }
-//
-//        let newView = currentViewController.view
-//        self.contentView = newView
-//
-//        let windowRect = self.frame
-//        let currentViewRect = newView.frame
-//
-//        let yPos = windowRect.origin.y + (windowRect.size.height - currentViewRect.size.height)
-//        let newFrame = NSRect(x: windowRect.origin.x, y: yPos, width: currentViewRect.size.width, height: currentViewRect.size.height)
-//
-//        self.setFrame(newFrame, display: true, animate: true)
-//    }
+    @objc func viewSelected(sender: NSToolbarItem) {
+        self.loadViewWithIdentifier(viewTabIdentifier: sender.itemIdentifier.rawValue, withAnimation: true)
+    }
+
+    func loadViewWithIdentifier(viewTabIdentifier: String, withAnimation shouldAnimate:Bool) {
+        guard currentView != viewTabIdentifier else { return }
+
+        currentView = viewTabIdentifier
+
+        var infoDictionary = [String: String]()
+
+        for dictionary in toolbarTabsArray {
+            if dictionary["identifier"] == viewTabIdentifier {
+                infoDictionary = dictionary
+                break
+            }
+        }
+
+        let className = infoDictionary["class"]!
+
+        switch className {
+        case "First":
+            currentViewController = First(nibName: NSNib.Name(rawValue: "First"), bundle: nil)
+
+        case "Second":
+            currentViewController = Second(nibName: NSNib.Name(rawValue: "Second"), bundle: nil)
+
+        case "Third":
+            currentViewController = Third(nibName: NSNib.Name(rawValue: "Third"), bundle: nil)
+
+        default:
+            break
+        }
+
+        let newView = currentViewController.view
+        self.contentView = newView
+
+        let windowRect = self.frame
+        let currentViewRect = newView.frame
+
+        let yPos = windowRect.origin.y + (windowRect.size.height - currentViewRect.size.height)
+        let newFrame = NSRect(x: windowRect.origin.x, y: yPos, width: currentViewRect.size.width, height: currentViewRect.size.height)
+
+        self.setFrame(newFrame, display: true, animate: true)
+    }
 }
