f117c85809c541b22e05a987c744b713b88f9bc9
kriglz <kristina.gelzinyte@gmail.com>
2020-12-23 13:06:03 -0800

    model setup

---
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Kristina Gelzinyte on 12/23/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.color.colorInvert() as? Color)
+    }
+}
+
+struct CardView_Preview: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
