643583efa22d32b624d61bfd807e4f3bdc2af53e
kriglz <kristina.gelzinyte@gmail.com>
2020-12-24 14:57:51 -0800

    accessibility and progress view

--- Scrumdinger/Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/Scrumdinger/MeetingHeaderView.swift
@@ -10,27 +10,9 @@ import SwiftUI
 struct MeetingHeaderView: View {
     @Binding var secondsElapsed: Int
     @Binding var secondsRemaining: Int
-    var body: some View {
-        VStack {
-            ProgressView(value: progress)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds elapsed")
-                        .font(.caption)
-                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds remaining")
-                        .font(.caption)
-                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
-                }
-            }
-        }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel(Text("Time remaining"))
-        .accessibilityValue(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
-    }
+    
+    let scrumColor: Color
+
     private var progress: Double {
         guard secondsRemaining > 0 else { return 1 }
         let totalSeconds = Double(secondsElapsed + secondsRemaining)
@@ -42,11 +24,38 @@ struct MeetingHeaderView: View {
     private var minutesRemainingMetric: String {
         minutesRemaining == 1 ? "minute" : "minutes"
     }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(scrumColor: scrumColor))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining")
+                        .font(.caption)
+                    HStack {
+                        Text("\(secondsRemaining)")
+                        Image(systemName: "hourglass.tophalf.fill")
+                    }
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time remaining"))
+        .accessibilityValue(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
+        .padding([.horizontal, .top])
+    }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView(secondsElapsed: .constant(60), secondsRemaining: .constant(180))
+        MeetingHeaderView(secondsElapsed: .constant(60), secondsRemaining: .constant(180), scrumColor: DailyScrum.data[0].color)
             .previewLayout(.sizeThatFits)
     }
 }
---
+++ Scrumdinger/Scrumdinger/ScrumProgressViewStyle.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by Kristina Gelzinyte on 12/24/20.
+//
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    var scrumColor: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(scrumColor.accessibleFontColor)
+                .frame(height: 20.0)
+            ProgressView(configuration)
+                .frame(height: 12.0)
+                .padding(.horizontal)
+        }
+    }
+}
