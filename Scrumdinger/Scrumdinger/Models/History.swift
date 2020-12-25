f0e860369871acd5a42764c93c45d6064b59bb71
kriglz <kristina.gelzinyte@gmail.com>
2020-12-25 13:46:40 -0800

    meeting history

---
+++ Scrumdinger/Scrumdinger/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Kristina Gelzinyte on 12/25/20.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [String]
+    var lengthInMinutes: Int
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
