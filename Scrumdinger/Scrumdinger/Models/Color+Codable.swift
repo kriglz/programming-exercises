a4df686d009e6d3fc2ee6fd06f0ff92bd24b2555
kriglz <kristina.gelzinyte@gmail.com>
2020-12-25 17:32:46 -0800

    new scrum model

---
+++ Scrumdinger/Scrumdinger/Models/Color+Codable.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Codable.swift
+//  Scrumdinger
+//
+//  Created by Kristina Gelzinyte on 12/25/20.
+//
+
+import SwiftUI
+
+struct Color_Codable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color_Codable_Previews: PreviewProvider {
+    static var previews: some View {
+        Color_Codable()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/Models/ScrumData.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumData.swift
+//  Scrumdinger
+//
+//  Created by Kristina Gelzinyte on 12/25/20.
+//
+
+import Foundation
+
+class ScrumData: ObservableObject {
+    private static var documentsFolder: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        } catch {
+            fatalError("Can't find documents directory.")
+        }
+    }
+    private static var fileURL: URL {
+        return documentsFolder.appendingPathComponent("scrums.data")
+    }
+    @Published var scrums: [DailyScrum] = []
+}
