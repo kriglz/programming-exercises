23baec329ad9bf189bde670974be113c5d6dcefe
kriglz <kristina.gelzinyte@gmail.com>
2020-12-25 17:39:25 -0800

    load and save

--- Scrumdinger/Scrumdinger/Models/ScrumData.swift
+++ Scrumdinger/Scrumdinger/Models/ScrumData.swift
@@ -19,4 +19,37 @@ class ScrumData: ObservableObject {
         return documentsFolder.appendingPathComponent("scrums.data")
     }
     @Published var scrums: [DailyScrum] = []
+    
+    func load() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.fileURL) else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.scrums = DailyScrum.data
+                }
+                #endif
+                return
+            }
+            
+            guard let dailyScrums = try? JSONDecoder().decode([DailyScrum].self, from: data) else {
+                fatalError("Can't decode saved scrum data.")
+            }
+            
+            DispatchQueue.main.async {
+                self?.scrums = dailyScrums
+            }
+        }
+    }
+    func save() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let scrums = self?.scrums else { fatalError("Self out of scope") }
+            guard let data = try? JSONEncoder().encode(scrums) else { fatalError("Error encoding data") }
+            do {
+                let outfile = Self.fileURL
+                try data.write(to: outfile)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
+    }
 }
